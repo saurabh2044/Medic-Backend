b72ba74f9f58dcc4896eb64b5c8283c3ea4e132d
Jo-ravar <ravijoshi0996@gmail.com>
2018-02-07 17:26:26 +0530

    Hosted online database

--- config/constants.js
+++ config/constants.js
@@ -1,6 +1,8 @@
 module.exports = {
   MONGO_DB: {
-    URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/saurabh',
+    URI:
+      process.env.MONGODB_URI ||
+      'mongodb://saurabh:password@ds227858.mlab.com:27858/medicdb',
   },
   SECRET_KEY: 'tp4JjBQNl01w+6FStJU1PHIKSI7RpofyqMelmm3x',
   ENUMS: {
